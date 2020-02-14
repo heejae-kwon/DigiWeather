86542d1e74ca6b421a026ba25c9c5e1bed9de798
heejae.kwon <sfhy33@gmail.com>
2020-02-14 15:13:44 -0800

    Updates

---
+++ precache-manifest.312bbf1acc8141c2fb91dd1b0fb7f4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588f8b724ca3d12547cc9e059b47dd34",
+    "url": "/DigiWeather/index.html"
+  },
+  {
+    "revision": "76d4b10ee7aa87a9c7aa",
+    "url": "/DigiWeather/static/css/main.dfcc4d0f.chunk.css"
+  },
+  {
+    "revision": "20a2b10ae66932d3cb92",
+    "url": "/DigiWeather/static/js/2.0c683380.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/DigiWeather/static/js/2.0c683380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d4b10ee7aa87a9c7aa",
+    "url": "/DigiWeather/static/js/main.55fde09c.chunk.js"
+  },
+  {
+    "revision": "c208455dbf6883318f0a",
+    "url": "/DigiWeather/static/js/runtime-main.728b05de.js"
+  }
+]);
