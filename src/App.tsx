682369d17057e234ecffccae90a9d376311e2fcc
heejae.kwon <sfhy33@gmail.com>
2020-03-13 21:53:23 -0700

    at least done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { SearchForm, SearchMethod } from './components/SearchForm';
 import WeatherForm from './components/WeatherForm';
-import Axios from 'axios';
 
 const App = () => {
 
-  const [weather, setWeather] = useState({});
+  const devUrl = "http://localhost:5000/api/weather/zipcode/";
+  const buildUrl = "http://ts-weather-server.herokuapp.com/api/weather/zipcode/";
+  const [weather, setWeather] = useState(Object);
+  const [forecast, setForecast] = useState(Object);
   const [isWeatherActive, setIsWeatherActive] = useState(false);
   const getWeather = async (content: string, searchMethod: SearchMethod) => {
     if (isWeatherActive === false) {
@@ -16,7 +19,8 @@ const App = () => {
       let response: any = null;
       switch (searchMethod) {
         case SearchMethod.ZIPCODE: {
-          response = await axios.get('https://ts-weather-server.herokuapp.com/api/weather/zipcode/' + content);
+          response = await axios.get(buildUrl + content);
+
           break;
         }
         case SearchMethod.Geographic: {
@@ -25,30 +29,75 @@ const App = () => {
           break;
         }
       }
-      setWeather(response.data);
-      console.log(response.data);
+      setWeather(response.data.weather);
+      setForecast(response.data.forecast);
     } catch (error) {
       console.error(error);
     }
 
   };
   const showWeather = () => {
-    if (isWeatherActive === false) {
+    if (isWeatherActive === false || !weather) {
       return;
     }
-    let content: string = '';
-    if (weather !== null) {
-      content = JSON.stringify(weather);
+    if (Object.keys(weather).length === 0) {
+      return;
     }
 
-    return <WeatherForm content={content} />;
-
+    return <WeatherForm main={weather.weather[0].main}
+      description={weather.weather[0].description}
+      temp={weather.main.temp}
+      temp_max={weather.main.temp_max}
+      temp_min={weather.main.temp_min}
+      humidity={weather.main.humidity}
+      wind_speed={weather.wind.speed}
+      dt={weather.dt}
+      name={weather.name}
+      icon={weather.weather[0].icon}
+    />
   };
+  const showForecast = () => {
+    if (isWeatherActive === false || !forecast) {
+      return;
+    }
+    if (Object.keys(forecast).length === 0) {
+      return;
+    }
+
+    const listOfForecast: Array<any> = forecast.list;
+    const res = listOfForecast.map((fc, index) => {
+      return (<div className="mx-2">
+        <WeatherForm key={index} main={fc.weather[0].main}
+          description={fc.weather[0].description}
+          temp={fc.main.temp}
+          temp_max={fc.main.temp_max}
+          temp_min={fc.main.temp_min}
+          humidity={fc.main.humidity}
+          wind_speed={fc.wind.speed}
+          dt={fc.dt}
+          name={""}
+          icon={fc.weather[0].icon}
+        />
+      </div>);
+    });
+    return (<div className="flex flex-col bg-blue-400 w-screen">
+      <div>
+        <p>
+          3hours forecast
+        </p>
+      </div>
+      <div className="flex flex-row">
+        {res}
+      </div>
+    </div>);
+  };
+
   return (
     <div className="bg-blue-400 h-screen w-screen">
       <div className="flex flex-col items-center py-4">
         <SearchForm getWeather={getWeather} />
         {showWeather()}
+        {showForecast()}
       </div>
     </div>
   );
--- src/components/WeatherForm.tsx
+++ src/components/WeatherForm.tsx
@@ -1,17 +1,62 @@
 import React from 'react';
 
 interface WeatherFormProps {
-  content: string;
+  main: string,
+  description: string,
+  temp: string,
+  temp_max: string,
+  temp_min: string,
+  humidity: string,
+  wind_speed: string,
+  dt: string,
+  name: string,
+  icon: string
 }
 const WeatherForm = (props: WeatherFormProps) => {
 
-  return (<div className="flex my-2 bg-white max-w-sm rounded overflow-scroll shadow-lg">
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">The Weather</div>
-      <p className="text-gray-700 text-base">
-        {props.content}
-      </p>
+  const imgSrc: string = "http://openweathermap.org/img/wn/" + props.icon + "@2x.png";
+  const time = new Date(parseInt(props.dt)*1000);
+
+  return (<div className="flex my-2 bg-white max-w-sm rounded shadow-lg">
+    <div className="flex flex-col items-center px-6 py-4">
+      <div className="flex flex-row font-bold text-2xl mb-2">
+        {props.name}
+      </div>
+      <div>
+        <img alt="weather img" src={imgSrc} />
+      </div>
+      <div>
+        <div className="flex flex-row justify-between font-bold text-lg mb-2">
+          <div className="mr-2">
+            {props.main}
+          </div>
+          <div className="ml-2">
+            {props.temp}
+          </div>
+        </div>
+        <p className="text-gray-700 text-base">
+          {time.toLocaleString()}
+        </p>
+        <div className="justify-between">
+          <p className="text-gray-700 text-base">
+            {"H: " + props.temp_max}
+          </p>
+          <p className="text-gray-700 text-base">
+            {"L: " + props.temp_min}
+          </p>
+        </div>
+        <p className="text-gray-700 text-base">
+          {"Humadity: " + props.humidity + "%"}
+        </p>
+        <p className="text-gray-700 text-base">
+          {"Wind: " + props.wind_speed}
+        </p>
+
+
+
+      </div>
     </div>
+
   </div>);
 }
 export default WeatherForm;
